6c992665748f7f8d2bc12d3c4ba01279f5cb8532
pcranaway <pcranaway@tuta.io>
2021-01-11 23:47:05 +0200

    add feature structure

---
+++ src/main/java/gg/lark/devenv/feature/Feature.java
@@ -0,0 +1,15 @@
+package gg.lark.devenv.feature;
+
+public abstract class Feature {
+
+    /**
+     * Enables the feature
+     */
+    public abstract void enable();
+
+    /**
+     * Disables the feature
+     */
+    public abstract void disable();
+
+}
