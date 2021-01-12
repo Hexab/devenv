d1a65d93369247c9c4f1814d544bd25f5d9c4de3
pcranaway <pcranaway@tuta.io>
2021-01-12 10:31:34 +0200

    add damage cause exceptions

--- src/main/java/gg/lark/devenv/feature/impl/NoDamageFeature.java
+++ src/main/java/gg/lark/devenv/feature/impl/NoDamageFeature.java
@@ -9,11 +9,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class NoDamageFeature extends Feature {
+
+    private static final List<EntityDamageEvent.DamageCause> ALLOWED_CAUSES = Arrays.asList(
+            EntityDamageEvent.DamageCause.CUSTOM,
+            EntityDamageEvent.DamageCause.LIGHTNING,
+            EntityDamageEvent.DamageCause.SUFFOCATION,
+            EntityDamageEvent.DamageCause.SUICIDE,
+            EntityDamageEvent.DamageCause.VOID
+    );
 
     private final Listener listener = new Listener() {
         @EventHandler
         public void onEntityDamage(EntityDamageEvent event) {
+            if(ALLOWED_CAUSES.contains(event.getCause())) return;
+
             event.setCancelled(true);
         }
 
